adf668b8bf1400c02adc76a3fb445035109e9cc6
Alex Desjardins <ardesjardins91@gmail.com>
2020-10-18 14:34:25 -0600

    Create factories

---
+++ spec/factories/items.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :item do
+    name {Faker::Coffee.blend_name}
+    description {Faker::Coffee.notes}
+    unit_price {Faker::Number.decimal(l_digits: 2)}
+    association :merchant_id
+    created_at {Faker::Date.between(from: '2010-01-01', to: '2020-01-01')}
+    updated_at {Faker::Date.between(from: '2010-01-01', to: '2020-01-01')}
+  end
+end
