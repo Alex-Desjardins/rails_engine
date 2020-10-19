e578a8691372a6b3967735a06ec56db862d2a768
Alex Desjardins <ardesjardins91@gmail.com>
2020-10-19 16:56:03 -0600

    Remove updated_at / created_at from models

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,8 +1,6 @@
 class Customer < ApplicationRecord
   validates_presence_of :first_name,
-                        :last_name,
-                        :created_at,
-                        :updated_at
+                        :last_name
 
   has_many :invoices
 end
