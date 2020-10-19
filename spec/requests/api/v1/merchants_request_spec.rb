dac63c09536f52aee2b679a78427b86e6a09bb15
Alex Desjardins <ardesjardins91@gmail.com>
2020-10-19 17:19:50 -0600

    Merchant crud functionality complete

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -5,8 +5,8 @@ describe 'Merchants REST Endpoints' do
     create_list(:merchant, 5)
 
     get '/api/v1/merchants'
-
     expect(response).to be_successful
+    expect(response.content_type).to eq("application/json")
 
     merchants = JSON.parse(response.body, symbolize_names: true)
 
@@ -14,5 +14,73 @@ describe 'Merchants REST Endpoints' do
     expect(merchants[:data].first).to have_key(:id)
     expect(merchants[:data].first).to have_key(:type)
     expect(merchants[:data].first).to have_key(:attributes)
+    expect(merchants[:data].first[:attributes]).to have_key(:name)
+  end
+
+  it "can return a merchant" do
+    merchant = create(:merchant)
+
+    get "/api/v1/merchants/#{merchant.id}"
+    expect(response).to be_successful
+    expect(response.content_type).to eq("application/json")
+
+    merchant_json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchant_json[:data][:id]).to eq(merchant.id.to_s)
+    expect(merchant_json[:data]).to have_key(:id)
+    expect(merchant_json[:data]).to have_key(:type)
+    expect(merchant_json[:data]).to have_key(:attributes)
+    expect(merchant_json[:data][:attributes]).to have_key(:name)
+  end
+
+  it "can create a merchant" do
+    merchant_params = { name: "Wilderness Exchange" }
+    headers = { "CONTENT_TYPE" => "application/json" }
+
+    post "/api/v1/merchants", params: JSON.generate(merchant_params), headers: headers
+    expect(response).to be_successful
+    expect(response.content_type).to eq("application/json")
+
+    merchant = Merchant.last
+    expect(merchant.name).to eq(merchant_params[:name])
+
+    merchant_json = JSON.parse(response.body, symbolize_names: true)
+    expect(merchant_json[:data][:id]).to eq(merchant.id.to_s)
+    expect(merchant_json[:data][:type]).to eq("merchant")
+    expect(merchant_json[:data][:attributes][:name]).to eq(merchant_params[:name])
+  end
+
+  it "can update a merchant" do
+    merchant = create(:merchant)
+    merchant_params = { name: "Wilderness Exchange" }
+    headers = { "CONTENT_TYPE" => "application/json" }
+    original_merchant = Merchant.last
+
+    put "/api/v1/merchants/#{merchant.id}", params: JSON.generate(merchant_params), headers: headers
+    expect(response).to be_successful
+    expect(response.content_type).to eq("application/json")
+
+    merchant = Merchant.find(merchant.id)
+
+    expect(merchant.name).to_not eq(original_merchant.name)
+    expect(merchant.name).to eq(merchant_params[:name])
+
+    merchant_json = JSON.parse(response.body, symbolize_names: true)
+    expect(merchant_json[:data][:id]).to eq(merchant.id.to_s)
+    expect(merchant_json[:data][:type]).to eq("merchant")
+    expect(merchant_json[:data][:attributes][:name]).to eq(merchant_params[:name])
+  end
+
+  it "can destroy a merchant" do
+    merchant = create(:merchant)
+
+    expect(Merchant.count).to eq(1)
+
+    delete "/api/v1/merchants/#{merchant.id}"
+
+    expect(Merchant.count).to eq(0)
+    expect(response).to be_successful
+
+    expect{Merchant.find(merchant.id)}.to raise_error(ActiveRecord::RecordNotFound)
   end
 end
