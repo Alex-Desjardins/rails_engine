c509713a6716adef02f18085fdaf357d1b17aa1e
Alex Desjardins <ardesjardins91@gmail.com>
2020-10-21 15:46:45 -0600

    Merchant Items Index Complete.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        get '/:id/items', to: 'items#index'
+      end
+
       resources :merchants, except: [:new, :edit]
       resources :items, except: [:new, :edit]
     end
